c0c2868641a4d8130c19d946516820d5cb4a6d21
deme-ctrl <demirewumanidefro@gmail.com>
2026-08-16 00:21:25 -0700

    feat(web): create ShipperDashboard view

---
+++ web/src/views/ShipperDashboard.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import WalletBalance from '../components/WalletBalance';
+import FreightOrderForm from '../components/FreightOrderForm';
+import ActiveShipment from '../components/ActiveShipment';
+
+export default function ShipperDashboard() {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <WalletBalance />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="lg:col-span-2">
+          <FreightOrderForm />
+        </div>
+        <div className="lg:col-span-1">
+          <ActiveShipment />
+        </div>
+      </div>
+    </div>
+  );
+}
