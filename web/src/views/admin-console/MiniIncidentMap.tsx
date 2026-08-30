bff73700ba40af7e413fa40b0e905b5d7ac28ff5
deme-ctrl <demirewumanidefro@gmail.com>
2026-08-29 23:25:04 -0700

    fix(web): switch MiniIncidentMap base layer to Esri Dark

--- web/src/views/admin-console/MiniIncidentMap.tsx
+++ web/src/views/admin-console/MiniIncidentMap.tsx
@@ -59,8 +59,8 @@ export default function MiniIncidentMap(props: MiniIncidentMapProps) {
         style={{ cursor: 'crosshair' }}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+          attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
         />
 
         <Polyline 
