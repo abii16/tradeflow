09dccf435f554d7797e5ae63b8035499f9e514e7
deme-ctrl <demirewumanidefro@gmail.com>
2026-09-03 01:32:09 -0700

    Darken ShipperPortal main background to make white cards stand out

--- web/src/views/shipper-portal/ShipperPortal.tsx
+++ web/src/views/shipper-portal/ShipperPortal.tsx
@@ -73,7 +73,7 @@ export default function ShipperPortal({ onSwitchPortal }: ShipperPortalProps) {
       <ShipperSidebar activeTab={activeTab} setActiveTab={setActiveTab} />
       <div className="flex-1 flex flex-col overflow-hidden">
         <UniversalTopBar />
-        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-6">
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-slate-100 p-6">
           {renderTab()}
         </main>
       </div>
